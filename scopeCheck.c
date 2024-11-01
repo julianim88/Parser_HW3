92699b1cef038c3fbaf3a4bb046527e47900ae9a
julianim88 <136631050+julianim88@users.noreply.github.com>
2024-11-01 17:35:48 -0400

    Create scopeCheck.c

---
+++ scopeCheck.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"//not sure if it will be needed here, but left it in case
+#include "id_attrs.h"
+#include "ast.h"
+#include "symtab.h"
+#include "scope_check.h"
+#include "utilities.h"
+#include "id_use.h"
