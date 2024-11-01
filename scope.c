1c5a9b2153ace7af3d7e4b906f0e6787697fb201
julianim88 <136631050+julianim88@users.noreply.github.com>
2024-11-01 17:38:08 -0400

    Create scope.c

---
+++ scope.c
@@ -0,0 +1,10 @@
+#include "scope.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
+#include "id_attrs.h"
+#include "ast.h"
+#include "symtab.h"
+#include "scope_check.h"
+#include "utilities.h"
+#include "id_use.h"
